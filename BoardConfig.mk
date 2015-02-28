3f47909e456599de3513e80c90d7243100347213
Omar Einea <eineao@gmail.com>
2015-02-28 14:14:22 +0100

    Revert "GCC: Update."
    This reverts commit 3df5528690598a55fb27a01b752dab2ce655585f.
    
    Change-Id: If45fac728ce37f9045fcea92e12903bc1bd19cd4
    Signed-off-by: Omar Einea <eineao@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,8 +27,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/amami/bluetooth
 
 # Kernel properties
 TARGET_KERNEL_CONFIG := slim_rhine_amami_row_defconfig
-TARGET_GCC_VERSION_ARM := 4.9-sm
-TARGET_GCC_VERSION_AND := 4.8-sm
 
 # External apps on SD
 TARGET_EXTERNAL_APPS = sdcard1
